001189d7449121ab77bbb0ab7170dfd91cfdc480
soybeanmilk <1101574955@qq.com>
2019-10-26 16:52:27 +0800

    add url

--- rest/rest-spi/src/main/java/com/github/yuanrw/im/rest/spi/domain/UserBase.java
+++ rest/rest-spi/src/main/java/com/github/yuanrw/im/rest/spi/domain/UserBase.java
@@ -6,6 +6,9 @@ package com.github.yuanrw.im.rest.spi.domain;
  *
  * @author yrw
  */
+
+// Java - 什么是领域模型(domain model)？贫血模型(anaemic domain model)和充血模型(rich domain model)有什么区别？
+// https://blog.csdn.net/troubleshooter/article/details/78479984
 public class UserBase {
 
     private String id;
